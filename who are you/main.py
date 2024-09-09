7cc4eb72ab0ae0d7c867410484ba4c582debadcf
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-09-09 10:33:39 -0600

    who are you

---
+++ who are you/main.py
@@ -0,0 +1,9 @@
+#Gavin Pierce, who are you
+
+name = input("what is your name?: ")
+
+age = input("what is your age?: ")
+
+color = input("what is your favorite color?: ")
+
+print ("so your name is", name, "your", age, "old, and your favorite color is", color, ".")
