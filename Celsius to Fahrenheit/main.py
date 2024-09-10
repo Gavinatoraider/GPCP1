a875d3ae12e6f6db8e0d9cd37747905bd80b10cd
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-09-10 12:36:45 -0600

    Celsius to Fahrenheit MK1

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,5 @@
+#Gavin Pierce, Celsius to Fahrenheit
+
+Celsius = 100
+
+print ((Celsius * 9/5)+32)
---
+++ variables/main.py
@@ -0,0 +1,8 @@
+#Gavin Pierce, awards tables
+
+staff= 23
+students= 100
+students= students -1
+guests=students*2
+seats =12
+print((staff+students+guests)/seats)
