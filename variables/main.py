36556bc5d018933f4b22773537ffd830e7ca7c99
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-09-12 10:13:31 -0600

    awards tables

--- variables/main.py
+++ variables/main.py
@@ -1,8 +1,11 @@
 #Gavin Pierce, awards tables
 
-staff= 23
+staff= 32
+staff= staff -3
 students= 100
 students= students -1
 guests=students*2
+guests= guests-15
 seats =12
-print((staff+students+guests)/seats)
+board =1
+print((board+staff+students+guests)/seats)
