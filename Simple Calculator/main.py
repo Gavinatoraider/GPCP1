8b4d5e99326215ebcddbb0fe4fe3fc070fa457f9
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-09-19 10:24:32 -0600

    madlib

---
+++ Simple Calculator/main.py
@@ -0,0 +1,12 @@
+#Gavin Pierce, Simple Calculator
+
+first= input("what is the first number you want to enter?:" )
+
+first= int(first)
+
+second= input("what is the second number you want to enter?:" )
+
+second= int(second)
+
+input("what do you want to do?(these are things like +, -, *, %.) ")
+
---
+++ madlib/pain.py
@@ -0,0 +1,25 @@
+person = input("give a name? " )
+
+age = input("give an age ")
+
+food = input("name a animal? " )
+
+lifeTime = input("name a year? " )
+
+home = input("where did they live? " )
+
+hobbie = input("name a hobbie ")
+
+pet = input("nmae a food " )
+
+activityWithPet = input("what do they do with there pet? " )
+
+nameOfFreind =input ("give a name " )
+
+freindTwo = input("give a name " )
+
+invention = input("name an item " )
+
+print (person,"an ", age, "year old, lived in ", home,"where they would ",hobbie,". ", person, "liked to eat cold", food, ". they had a pet ", pet, "they would", activityWithPet, ". they had ", age, "freinds, their best 2 freinds were ", nameOfFreind, "and")
+
+print (freindTwo, ". ", person, "lived in", lifeTime, "they had flying car and the new invetion that changed the world this was a", invention, ".")
