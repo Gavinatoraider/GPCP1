9bcac8225151896c916171acbf326c971167e093
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-09-24 10:31:19 -0600

    personal information converter

---
+++ personal Information Converter/main.py
@@ -0,0 +1,19 @@
+#Gavin Pierce, Personal Information Converter
+
+name = input("whats your name? ")
+
+age =  input("how old are you? ")
+
+height= input("whats your height in meters? ")
+
+number= input("whats your favorite number? ")
+
+print("so your ", name, "your ", age, "years old. your", height , "and your favorite number is ", number)
+
+number= int(number)
+
+age = int(age)
+
+height = float(height)
+
+print("so your ", name, "your ", age, "years old. your", height , "and your favorite number is ", number)
