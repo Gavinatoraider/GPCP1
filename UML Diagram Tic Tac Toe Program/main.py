77be1ebbd6f98746049793b1c6d1de6a8fa2c356
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-01 10:34:24 -0600

    Diagram your morning routine

---
+++ UML Diagram Tic Tac Toe Program/main.py
@@ -0,0 +1,3 @@
+#Gavin Pierce UML Diagram Tic Tac Toe Program
+
+print("welcom to tic tac toe")
---
+++ UML Diagram your morning routine/main.py
@@ -0,0 +1,9 @@
+#Gavin Pierce UML Diagram your morning routine
+
+getDressed=print("I first get dressed")
+fixHair=print("I fix my hair")
+eatBreakfest=print("I eat breakfest")
+brushTeeth=print("I brush teeth get ready the rest of the way")
+rideToBus=print("I ride to bus")
+
+print("when I get ready in the morning I first ",getDressed, " then ",fixHair, " then ",eatBreakfest, " then ", brushTeeth, "then ", rideToBus, "thats my morning routine" )
