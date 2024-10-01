df793cc975287741ba57f3813ee1bb25baa191b3
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-01 10:49:49 -0600

    making your bed

---
+++ UML Diagram for making your bed/main.py
@@ -0,0 +1,13 @@
+#Gavin Pierce UML Diagram your morning routine
+stepOne="take things like pillows off of bed"
+
+stepTwo="pull blanket to the top of bed"
+
+stepthree="pull blanket down to smooth wrinkels"
+
+stepFour="then out pillow back on bed nicely"
+
+stepFive="then fold extra blankets and put on fron of bed"
+
+print("making your bed is simple if you are using this to learn and you are older then five years old there are bigger problems anyways, first, ",stepOne," then, ",stepTwo," ,third, ",stepthree," ,at that point, ",stepFour," ,lastly, ",stepFive,". thanks for coming")
+print("to, how to make your bed.")
