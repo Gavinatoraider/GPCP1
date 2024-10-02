c8c7fffad3e8b0531c136f012742bfc379947d09
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-02 10:03:49 -0600

    UML Diagram your morning routine mkII

--- UML Diagram your morning routine/main.py
+++ UML Diagram your morning routine/main.py
@@ -1,9 +1,9 @@
 #Gavin Pierce UML Diagram your morning routine
 
-getDressed=print("I first get dressed")
-fixHair=print("I fix my hair")
-eatBreakfest=print("I eat breakfest")
-brushTeeth=print("I brush teeth get ready the rest of the way")
-rideToBus=print("I ride to bus")
+getDressed=("I first get dressed")
+fixHair=("I fix my hair")
+eatBreakfest=("I eat breakfest")
+brushTeeth=("I brush teeth get ready the rest of the way")
+rideToBus=("I ride to bus")
 
-print("when I get ready in the morning I first ",getDressed, " then ",fixHair, " then ",eatBreakfest, " then ", brushTeeth, "then ", rideToBus, "thats my morning routine" )
+print("when I get ready in the morning ",getDressed, " then ",fixHair, " then ",eatBreakfest, " then ", brushTeeth, "then ", rideToBus, "thats my morning routine" )
