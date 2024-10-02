797c6ac73174ad019ea4cd7c766fe80f76bf9deb
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-02 10:41:18 -0600

    UML Diagram cooking your favorite food mkI

---
+++ UML Diagram cooking your favorite food/main.py
@@ -0,0 +1,16 @@
+#Gavin Pierce UML Diagram cooking your favorite food
+
+stepOne="get peanut butter"
+
+stepTwo="get jam/jelly"
+
+stepthree="get two pieces of bread"
+
+stepFour="put peanut butter on one piece of bread"
+
+stepFive="put jelly/jam on the other piece of bread"
+
+stepSix="put bread piece one on bread piece two"
+
+print("to make a peanut butter and jelly sandwich you will need to, first ",stepOne," then, ",stepTwo," ,third, ",stepthree," ,at that point, ",stepFour," ,after that, ",stepFive, "lastly ",stepSix ,". thanks for coming")
+print("to, how to make Food.")
