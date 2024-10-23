a44ce81d5bd30b9b8067b77909fc472f78b5500d
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-23 10:33:35 -0600

    Average Grade MKI

---
+++ Average Grade/main.py
@@ -0,0 +1,23 @@
+#gavin Pierce Average Grade
+
+print ("this program will take each of your classes grades and avrages them.")
+#first grade
+first= int(input("what is your first classes grade? "))
+#second grade
+second = int(input("what is your second classes grade? "))
+# third grade
+third = int(input("what is your third classes grade? "))
+#fourth grade
+fourth = int(input("what is your fourth classes grade? "))
+#fith grade
+fith = int(input("what is your fith classes grade? "))
+#sixth grade
+sixth = int(input("what is your sixth classes grade? "))
+#seventh grade
+seventh = int(input("what is your seventh classes grade? "))
+#eighth grade
+eighth = int(input("what is your eighth classes grade? "))
+#Addes grades
+AddedGrade= (first + second + third + fourth + fith + sixth + seventh + eighth)
+#avreages grades
+print ("your average grade is ",AddedGrade / 8,".")
