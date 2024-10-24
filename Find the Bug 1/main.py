c0073f1bc609b90ad7c7c35f456ce7c3a63ab2fc
Gavinatoraider <gaivn.pierce@ucas-edu.net>
2024-10-24 10:22:03 -0600

    Find the Bug 1 MKI

---
+++ Find the Bug 1/main.py
@@ -0,0 +1,7 @@
+#Gavin Pierce Find the Bug 1
+
+# The function is supposed to calculate the area of a rectangle, but it throws an error. Find the bug.
+def calculate_area(length, width):
+    return length * width  
+#What was the problem with the code? length was spelled wrong it was spelled lenght
+#What type of error is it? (Syntax, Logic, or Runtime) it was a syntax error
