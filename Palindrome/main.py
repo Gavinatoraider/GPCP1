ed113e860d9d10d9ed3d499dba43ea99604505ec
Gavinatoraider <gavin.pierce@ucas-edu.net>
2024-11-08 10:50:55 -0700

    secret cypher incomplete

---
+++ Palindrome/main.py
@@ -0,0 +1,11 @@
+#Gavin Pierce Palindrome
+
+palindrome= input("give me a word.")
+repal = palindrome
+if palindrom:
+
+     your code that outputs that is is a panendrom
+
+else:
+
+     print("This word isn't a palindrome") 
