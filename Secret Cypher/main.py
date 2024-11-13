953bbec9991203f8c3e90f0450cae2d6a9b439fd
Gavinatoraider <gavin.pierce@ucas-edu.net>
2024-11-13 10:25:08 -0700

    secret cypher I finaly finished it!!!

--- Secret Cypher/main.py
+++ Secret Cypher/main.py
@@ -1,33 +1,18 @@
 # Gavin Pierce Secret Cypher
 
 word=input("what is the word that you would like to use? ")
-var= letter+1
-shift= var+1  
-letter= 
-A=1
-B=2
-C=3
-D=4
-E=5
-F=6
-G=7
-H=8
-I=9
-J=10
-K=11
-L=12
-M=13
-N=14
-O=15
-P=16
-Q=17
-R=18
-S=19
-T=20
-U=21
-V=22
-W=23
-X=24
-Y=25
-Z=26 
-print (word)
+#empty string is mean to hold the values
+cypher=""
+def shift(word, cypher):
+    for letter in word:
+         #takes the word and converts it to numbers
+        letter=ord(letter)
+    #adds value
+        letter+=1
+        letter=chr(letter)
+        #converts back to word
+        cypher+=letter
+    print(cypher)    
+
+shift(word,cypher)
+
