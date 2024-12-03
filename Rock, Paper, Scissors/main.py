09dff7ebeac9f6403f13866cc2fa9ac48617be94
Gavinatoraider <gavin.pierce@ucas-edu.net>
2024-12-03 10:13:30 -0700

    rock paper scissors Game MKI

--- Rock, Paper, Scissors/main.py
+++ Rock, Paper, Scissors/main.py
@@ -8,42 +8,64 @@ likeToPlay=input("hi there I am the rock paper scissors bot would you like to pl
 if likeToPlay == "yes":
     print("ok" )
     playAgain="yes"
-    while playAgain == "yes":
+    while likeToPlay == "yes":
         playChoise=input("what would you like to chose? rock, paper or sicssors? ")
         if playChoise in playChoiseList:
             aiTurn= random.choice(playChoiseList)
+
+            
             if playChoise == "rock": 
-                if aiTurn == "scicssors":
+                if aiTurn == "sicssors":
                     print("you won")
                     playerScore += 1
                     print("the score is", playerScore, "to", aiScore)
-                
-                elif print("you tied"):
-                     print("the score is", playerScore, "to", aiScore)
-                else: print("the ai won")
-                aiScore += 1    
+                    likeToPlay=input("would you like to play again? ")
+                elif aiTurn == "rock": 
+                    print("you tied")
+                    print("the score is", playerScore, "to", aiScore)
+                    likeToPlay=input("would you like to play again? ")
+                else:
+                    print("the ai won")
+                    aiScore += 1
+                    print("the score is", playerScore, "to", aiScore)
+                    likeToPlay=input("would you like to play again? ")
+
 
             if playChoise == "paper": 
                 if aiTurn == "rock":
                     print("you won")
                     playerScore += 1
                     print("the score is", playerScore, "to", aiScore)
-                elif print("you tied"):
+                    likeToPlay=input("would you like to play again? ")
 
+                elif aiTurn == "paper":
+                     print("you tied")
                      print("the score is", playerScore, "to", aiScore)
-                else: print("the ai won")
-                aiScore += 1         
+                     likeToPlay=input("would you like to play again? ")
+                    
+                else: 
+                    print("the ai won")
+                    aiScore += 1
+                print("the score is", playerScore, "to", aiScore)         
+                likeToPlay=input("would you like to play again? ")
+
 
             if playChoise == "sicssors": 
                 if aiTurn == "paper":
                     print("you won")
                     playerScore += 1
                     print("the score is", playerScore, "to", aiScore)
-                elif print("you tied"):
+                    likeToPlay=input("would you like to play again? ")
 
-                     print("the score is", playerScore, "to", aiScore)                
-                else: print("the ai won")
-                aiScore += 1                        
+                elif aiTurn == "sicssors":
+                    print("you tied")
+                    print("the score is", playerScore, "to", aiScore)
+                    likeToPlay=input("would you like to play again? ")
                                 
-        else: print("you need a valid varible")
+                else: 
+                    print("the ai won")
+                    aiScore += 1
+                    print("the score is", playerScore, "to", aiScore)
+                    likeToPlay=input("would you like to play again? ")    
+  
 else: print("OK bye :(")
