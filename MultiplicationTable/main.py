8808d027e17ac6a87e741ba66fd18c323c100354
Gavinatoraider <gavin.pierce@ucas-edu.net>
2024-12-03 10:28:39 -0700

    Multiplication Table

--- MultiplicationTable/main.py
+++ MultiplicationTable/main.py
@@ -1,8 +1,7 @@
 #Gavin Pierce Multiplication Table
 
 number=int(input("what number would you like to get multiples for? "))
-
-numberList= ["1","2","3","4","5","6","7","8","9","10","11","12"]
-
-for i in numberList :
-    print (number*numberList)
+numberMultiplied=(0)
+for i in range(14):
+    print (numberMultiplied*number)
+    numberMultiplied+=1
